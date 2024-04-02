e5e37c513af143d4a16ea1e046294e85aba6c290
Christian Hapke <chris.github@hapke.com>
2024-04-02 16:59:01 +0200

    Implemented publishing to Maven Central (https://central.sonatype.com/)

---
+++ gradlew-publishAndReleaseToMavenCentral.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# required environment variables:
+# - ORG_GRADLE_PROJECT_signingInMemoryKey
+# - ORG_GRADLE_PROJECT_signingInMemoryKeyPassword
+# - ORG_GRADLE_PROJECT_mavenCentralUsername
+# - ORG_GRADLE_PROJECT_mavenCentralPassword
+
+# check if the required environment variables are set
+if [ -z "$ORG_GRADLE_PROJECT_signingInMemoryKey" ]; then
+  echo "ERROR: Environment variable ORG_GRADLE_PROJECT_signingInMemoryKey is not set but required"
+  exit 1
+fi
+if [ -z "$ORG_GRADLE_PROJECT_signingInMemoryKeyPassword" ]; then
+  echo "ERROR: Environment variable ORG_GRADLE_PROJECT_signingInMemoryKeyPassword is not set but required"
+  exit 1
+fi
+if [ -z "$ORG_GRADLE_PROJECT_mavenCentralUsername" ]; then
+  echo "ERROR: Environment variable ORG_GRADLE_PROJECT_mavenCentralUsername is not set but required"
+  exit 1
+fi
+if [ -z "$ORG_GRADLE_PROJECT_mavenCentralPassword" ]; then
+  echo "ERROR: Environment variable ORG_GRADLE_PROJECT_mavenCentralPassword is not set but required"
+  exit 1
+fi
+
+# publish to Maven Central
+./gradlew clean build publishAndReleaseToMavenCentral --no-configuration-cache --info
